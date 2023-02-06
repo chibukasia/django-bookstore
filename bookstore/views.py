4c74178602254338750a9407b2f7839864883db0
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-06 23:45:44 +0300

    added the book view classes

--- bookstore/views.py
+++ bookstore/views.py
@@ -45,3 +45,45 @@ class AuthorDetails(APIView):
         author = self.get_author(pk)
         author.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class BookList(APIView):
+    def get (self, request, format=None):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BookDetails(APIView):
+    
+    def get_book(self, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+            return book
+        except Book.DoesNotExist:
+            raise Http404
+        
+    
+    def get(self, request, pk, format=None):
+        book = self.get_book(pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def patch(self, request, pk, format=None):
+        book = self.get_book(pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        book = self.get_book(pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
