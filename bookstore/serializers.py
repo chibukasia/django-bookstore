6a2fefd4bbafcba9af345cdc1af9fb8f7574bd0f
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-06 12:17:53 +0300

    added model serializers

---
+++ bookstore/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from models import Book, Author
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['name'] 
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['title', 'isbn', 'created', 'author', 'excerpt', 'pub_date'] 
