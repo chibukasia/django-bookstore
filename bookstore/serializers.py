40f05042c702fb789af6e8613a7feeee46b9bfac
Chibukasia Nelson <chibukasianelson@gmail.com>
2023-02-07 23:29:30 +0300

    included id in serializer fields

--- bookstore/serializers.py
+++ bookstore/serializers.py
@@ -4,10 +4,10 @@ from .models import Book, Author
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ['name'] 
+        fields = ['id','name'] 
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ['title', 'isbn', 'created', 'author', 'excerpt', 'pub_date'] 
+        fields = ['id','title', 'isbn', 'created', 'author', 'excerpt', 'pub_date'] 
